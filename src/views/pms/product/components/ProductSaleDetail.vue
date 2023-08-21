87fb739953230a4aac77359aa1a502ab2414b324
王秋伟 <1002392634@qq.com>
2023-08-21 18:36:05 +0800

    修改显示

--- src/views/pms/product/components/ProductSaleDetail.vue
+++ src/views/pms/product/components/ProductSaleDetail.vue
@@ -1,6 +1,12 @@
 <template>
   <div style="margin-top: 50px">
-    <el-form :model="value" ref="productSaleForm" label-width="120px" class="form-inner-container" size="small">
+    <el-form
+      :model="value"
+      ref="productSaleForm"
+      label-width="120px"
+      class="form-inner-container"
+      size="small"
+    >
       <el-form-item label="赠送积分：">
         <el-input v-model="value.giftPoint"></el-input>
       </el-form-item>
@@ -14,14 +20,16 @@
         <el-switch
           v-model="value.previewStatus"
           :active-value="1"
-          :inactive-value="0">
+          :inactive-value="0"
+        >
         </el-switch>
       </el-form-item>
       <el-form-item label="商品上架：">
         <el-switch
           v-model="value.publishStatus"
           :active-value="1"
-          :inactive-value="0">
+          :inactive-value="0"
+        >
         </el-switch>
       </el-form-item>
       <el-form-item label="商品推荐：">
@@ -29,13 +37,15 @@
         <el-switch
           v-model="value.newStatus"
           :active-value="1"
-          :inactive-value="0">
+          :inactive-value="0"
+        >
         </el-switch>
-        <span style="margin-left: 10px;margin-right: 10px">推荐</span>
+        <span style="margin-left: 10px; margin-right: 10px">推荐</span>
         <el-switch
           v-model="value.recommandStatus"
           :active-value="1"
-          :inactive-value="0">
+          :inactive-value="0"
+        >
         </el-switch>
       </el-form-item>
       <el-form-item label="服务保证：">
@@ -55,7 +65,11 @@
         <el-input v-model="value.keywords"></el-input>
       </el-form-item>
       <el-form-item label="商品备注：">
-        <el-input v-model="value.note" type="textarea" :autoSize="true"></el-input>
+        <el-input
+          v-model="value.note"
+          type="textarea"
+          :autoSize="true"
+        ></el-input>
       </el-form-item>
       <el-form-item label="选择优惠方式：">
         <el-radio-group v-model="value.promotionType" size="small">
@@ -66,14 +80,15 @@
           <el-radio-button :label="4">满减价格</el-radio-button>
         </el-radio-group>
       </el-form-item>
-      <el-form-item v-show="value.promotionType===1">
+      <el-form-item v-show="value.promotionType === 1">
         <div>
           开始时间：
           <el-date-picker
             v-model="value.promotionStartTime"
             type="datetime"
             :picker-options="pickerOptions1"
-            placeholder="选择开始时间">
+            placeholder="选择开始时间"
+          >
           </el-date-picker>
         </div>
         <div class="littleMargin">
@@ -82,225 +97,246 @@
             v-model="value.promotionEndTime"
             type="datetime"
             :picker-options="pickerOptions1"
-            placeholder="选择结束时间">
+            placeholder="选择结束时间"
+          >
           </el-date-picker>
         </div>
         <div class="littleMargin">
           促销价格：
-          <el-input style="width: 220px" v-model="value.promotionPrice" placeholder="输入促销价格"></el-input>
+          <el-input
+            style="width: 220px"
+            v-model="value.promotionPrice"
+            placeholder="输入促销价格"
+          ></el-input>
         </div>
-
       </el-form-item>
-      <el-form-item v-show="value.promotionType===2">
-        <div v-for="(item, index) in value.memberPriceList" :class="{littleMargin:index!==0}">
-          {{item.memberLevelName}}：
+      <el-form-item v-show="value.promotionType === 2">
+        <div
+          v-for="(item, index) in value.memberPriceList"
+          :class="{ littleMargin: index !== 0 }"
+        >
+          {{ item.memberLevelName }}：
           <el-input v-model="item.memberPrice" style="width: 200px"></el-input>
         </div>
       </el-form-item>
-      <el-form-item v-show="value.promotionType===3">
-        <el-table :data="value.productLadderList"
-                  style="width: 80%" border>
-          <el-table-column
-            label="数量"
-            align="center"
-            width="120">
+      <el-form-item v-show="value.promotionType === 3">
+        <el-table :data="value.productLadderList" style="width: 80%" border>
+          <el-table-column label="数量" align="center" width="120">
             <template slot-scope="scope">
               <el-input v-model="scope.row.count"></el-input>
             </template>
           </el-table-column>
-          <el-table-column
-            label="折扣"
-            align="center"
-            width="120">
+          <el-table-column label="折扣" align="center" width="120">
             <template slot-scope="scope">
               <el-input v-model="scope.row.discount"></el-input>
             </template>
           </el-table-column>
-          <el-table-column
-            align="center"
-            label="操作">
+          <el-table-column align="center" label="操作">
             <template slot-scope="scope">
-              <el-button type="text" @click="handleRemoveProductLadder(scope.$index, scope.row)">删除</el-button>
-              <el-button type="text" @click="handleAddProductLadder(scope.$index, scope.row)">添加</el-button>
+              <el-button
+                type="text"
+                @click="handleRemoveProductLadder(scope.$index, scope.row)"
+                >删除</el-button
+              >
+              <el-button
+                type="text"
+                @click="handleAddProductLadder(scope.$index, scope.row)"
+                >添加</el-button
+              >
             </template>
           </el-table-column>
         </el-table>
       </el-form-item>
-      <el-form-item v-show="value.promotionType===4">
-        <el-table :data="value.productFullReductionList"
-                  style="width: 80%" border>
-          <el-table-column
-            label="满"
-            align="center"
-            width="120">
+      <el-form-item v-show="value.promotionType === 4">
+        <el-table
+          :data="value.productFullReductionList"
+          style="width: 80%"
+          border
+        >
+          <el-table-column label="满" align="center" width="120">
             <template slot-scope="scope">
               <el-input v-model="scope.row.fullPrice"></el-input>
             </template>
           </el-table-column>
-          <el-table-column
-            label="立减"
-            align="center"
-            width="120">
+          <el-table-column label="立减" align="center" width="120">
             <template slot-scope="scope">
               <el-input v-model="scope.row.reducePrice"></el-input>
             </template>
           </el-table-column>
-          <el-table-column
-            align="center"
-            label="操作">
+          <el-table-column align="center" label="操作">
             <template slot-scope="scope">
-              <el-button type="text" @click="handleRemoveFullReduction(scope.$index, scope.row)">删除</el-button>
-              <el-button type="text" @click="handleAddFullReduction(scope.$index, scope.row)">添加</el-button>
+              <el-button
+                type="text"
+                @click="handleRemoveFullReduction(scope.$index, scope.row)"
+                >删除</el-button
+              >
+              <el-button
+                type="text"
+                @click="handleAddFullReduction(scope.$index, scope.row)"
+                >添加</el-button
+              >
             </template>
           </el-table-column>
         </el-table>
       </el-form-item>
       <el-form-item style="text-align: center">
-        <el-button size="medium" @click="handlePrev">上一步，填写商品信息</el-button>
-        <el-button type="primary" size="medium" @click="handleNext">下一步，填写商品属性</el-button>
+        <el-button size="medium" @click="handlePrev"
+          >上一步，填写商品信息</el-button
+        >
+        <el-button type="primary" size="medium" @click="handleNext"
+          >下一步，填写商品属性</el-button
+        >
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
-  import {fetchList as fetchMemberLevelList} from '@/api/memberLevel'
+import { fetchList as fetchMemberLevelList } from "@/api/memberLevel";
 
-  export default {
-    name: "ProductSaleDetail",
-    props: {
-      value: Object,
-      isEdit: {
-        type: Boolean,
-        default: false
-      }
+export default {
+  name: "ProductSaleDetail",
+  props: {
+    value: Object,
+    isEdit: {
+      type: Boolean,
+      default: false,
     },
-    data() {
-      return {
-        //日期选择器配置
-        pickerOptions1: {
-          disabledDate(time) {
-            return time.getTime() < Date.now();
-          }
+  },
+  data() {
+    return {
+      //日期选择器配置
+      pickerOptions1: {
+        disabledDate(time) {
+          return time.getTime() < Date.now();
+        },
+      },
+    };
+  },
+  created() {
+    if (this.isEdit) {
+      // this.handleEditCreated();
+    } else {
+      fetchMemberLevelList({ defaultStatus: 0 }).then((response) => {
+        let memberPriceList = [];
+        for (let i = 0; i < response.data.length; i++) {
+          let item = response.data[i];
+          memberPriceList.push({
+            memberLevelId: item.id,
+            memberLevelName: item.name,
+          });
         }
+        this.value.memberPriceList = memberPriceList;
+      });
+    }
+  },
+  computed: {
+    //选中的服务保证
+    selectServiceList: {
+      get() {
+        let list = [];
+        if (
+          this.value.serviceIds === undefined ||
+          this.value.serviceIds == null ||
+          this.value.serviceIds === ""
+        )
+          return list;
+        let ids = this.value.serviceIds.split(",");
+        for (let i = 0; i < ids.length; i++) {
+          list.push(Number(ids[i]));
+        }
+        return list;
+      },
+      set(newValue) {
+        let serviceIds = "";
+        if (newValue != null && newValue.length > 0) {
+          for (let i = 0; i < newValue.length; i++) {
+            serviceIds += newValue[i] + ",";
+          }
+          if (serviceIds.endsWith(",")) {
+            serviceIds = serviceIds.substr(0, serviceIds.length - 1);
+          }
+          this.value.serviceIds = serviceIds;
+        } else {
+          this.value.serviceIds = null;
+        }
+      },
+    },
+  },
+  methods: {
+    handleEditCreated() {
+      let ids = this.value.serviceIds.split(",");
+      console.log("handleEditCreated", ids);
+      for (let i = 0; i < ids.length; i++) {
+        this.selectServiceList.push(Number(ids[i]));
       }
     },
-    created() {
-      if (this.isEdit) {
-        // this.handleEditCreated();
+    handleRemoveProductLadder(index, row) {
+      let productLadderList = this.value.productLadderList;
+      if (productLadderList.length === 1) {
+        productLadderList.pop();
+        productLadderList.push({
+          count: 0,
+          discount: 0,
+          price: 0,
+        });
       } else {
-        fetchMemberLevelList({defaultStatus: 0}).then(response => {
-          let memberPriceList = [];
-          for (let i = 0; i < response.data.length; i++) {
-            let item = response.data[i];
-            memberPriceList.push({memberLevelId: item.id, memberLevelName: item.name})
-          }
-          this.value.memberPriceList = memberPriceList;
+        productLadderList.splice(index, 1);
+      }
+    },
+    handleAddProductLadder(index, row) {
+      let productLadderList = this.value.productLadderList;
+      if (productLadderList.length < 3) {
+        productLadderList.push({
+          count: 0,
+          discount: 0,
+          price: 0,
+        });
+      } else {
+        this.$message({
+          message: "最多只能添加三条",
+          type: "warning",
         });
       }
     },
-    computed: {
-      //选中的服务保证
-      selectServiceList: {
-        get() {
-          let list = [];
-          if (this.value.serviceIds === undefined || this.value.serviceIds == null || this.value.serviceIds === '') return list;
-          let ids = this.value.serviceIds.split(',');
-          for (let i = 0; i < ids.length; i++) {
-            list.push(Number(ids[i]));
-          }
-          return list;
-        },
-        set(newValue) {
-          let serviceIds = '';
-          if (newValue != null && newValue.length > 0) {
-            for (let i = 0; i < newValue.length; i++) {
-              serviceIds += newValue[i] + ',';
-            }
-            if (serviceIds.endsWith(',')) {
-              serviceIds = serviceIds.substr(0, serviceIds.length - 1)
-            }
-            this.value.serviceIds = serviceIds;
-          } else {
-            this.value.serviceIds = null;
-          }
-        }
+    handleRemoveFullReduction(index, row) {
+      let fullReductionList = this.value.productFullReductionList;
+      if (fullReductionList.length === 1) {
+        fullReductionList.pop();
+        fullReductionList.push({
+          fullPrice: 0,
+          reducePrice: 0,
+        });
+      } else {
+        fullReductionList.splice(index, 1);
       }
     },
-    methods: {
-      handleEditCreated() {
-        let ids = this.value.serviceIds.split(',');
-        console.log('handleEditCreated', ids);
-        for (let i = 0; i < ids.length; i++) {
-          this.selectServiceList.push(Number(ids[i]));
-        }
-      },
-      handleRemoveProductLadder(index, row) {
-        let productLadderList = this.value.productLadderList;
-        if (productLadderList.length === 1) {
-          productLadderList.pop();
-          productLadderList.push({
-            count: 0,
-            discount: 0,
-            price: 0
-          })
-        } else {
-          productLadderList.splice(index, 1);
-        }
-      },
-      handleAddProductLadder(index, row) {
-        let productLadderList = this.value.productLadderList;
-        if (productLadderList.length < 3) {
-          productLadderList.push({
-            count: 0,
-            discount: 0,
-            price: 0
-          })
-        } else {
-          this.$message({
-            message: '最多只能添加三条',
-            type: 'warning'
-          });
-        }
-      },
-      handleRemoveFullReduction(index, row) {
-        let fullReductionList = this.value.productFullReductionList;
-        if (fullReductionList.length === 1) {
-          fullReductionList.pop();
-          fullReductionList.push({
-            fullPrice: 0,
-            reducePrice: 0
-          });
-        } else {
-          fullReductionList.splice(index, 1);
-        }
-      },
-      handleAddFullReduction(index, row) {
-        let fullReductionList = this.value.productFullReductionList;
-        if (fullReductionList.length < 3) {
-          fullReductionList.push({
-            fullPrice: 0,
-            reducePrice: 0
-          });
-        } else {
-          this.$message({
-            message: '最多只能添加三条',
-            type: 'warning'
-          });
-        }
-      },
-      handlePrev() {
-        this.$emit('prevStep')
-      },
-      handleNext() {
-        this.$emit('nextStep')
+    handleAddFullReduction(index, row) {
+      let fullReductionList = this.value.productFullReductionList;
+      if (fullReductionList.length < 3) {
+        fullReductionList.push({
+          fullPrice: 0,
+          reducePrice: 0,
+        });
+      } else {
+        this.$message({
+          message: "最多只能添加三条",
+          type: "warning",
+        });
       }
-    }
-  }
+    },
+    handlePrev() {
+      this.$emit("prevStep");
+    },
+    handleNext() {
+      this.$emit("nextStep");
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .littleMargin {
-    margin-top: 10px;
-  }
+.littleMargin {
+  margin-top: 10px;
+}
 </style>
